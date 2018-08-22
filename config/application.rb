3b0eb07eeee71edaf5f2e4f8791aada9e1ba4f15
Gorlex <gorlexx@hotmail.com>
2018-08-21 21:33:22 -0500

    API funcional

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,13 @@ Bundler.require(*Rails.groups)
 module TallerFacturas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+      config.load_defaults 5.2
+        config.middleware.insert_before 0, Rack::Cors do
+        allow do
+        origins '*' #se colocan las URL q se quiera q tengan acceso
+        resource '*', headers: :any, methods: [:get,:patchm, :put, :delete, :post, :options]
+        end
+      end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
